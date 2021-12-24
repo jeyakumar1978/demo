79023aea179cf067769e0bb58a0705b1decdeb86
Jeyakumar I <jeyakumar@Jeyakumars-MacBook-Air.local>
2021-12-24 12:58:14 +0530

    yaml file changed

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController
     @RequestMapping("/hello")
     public String hello()
     {
-        return "Hello from Mervin";
+        return "Hello from Mervin Jose";
     }
 
     @RequestMapping("/helloworld")
